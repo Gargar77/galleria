d608b9d2bc9aa52ac3001e13d15c7170b09362ce
gargar77 <gbautista2@knights.ucf.edu>
2021-07-30 13:20:44 -0400

    connected homepage to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
+import './pages/homepage/homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+
 function App() {
   return (
     <div className="App">
+      <Homepage/>
     </div>
   );
 }
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+ import React from 'react';
+
+ import './homepage.styles.scss';
+
+
+ class Homepage extends React.Component {
+     render() {
+         return (
+             <main className="homepage">Homepage</main>
+         )
+     }
+ }
+
+ export default Homepage;
