23950b9c45500884c2735b25210ae69f881fee11
gargar77 <gbautista2@knights.ucf.edu>
2021-07-31 16:27:56 -0400

    nav button routes to first artwork in  gallery

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import './nav.styles.css';
+import { Link } from 'react-router-dom';
 import {ReactComponent as Logo } from '../../assets/shared/logo.svg';
 
 const Nav = () => (
     <div className="nav-container">
         <nav className="main-nav">
             <Logo/>
-            <button>start Slideshow</button>
+            <Link to="/artwork?id=0">
+                <button>start Slideshow</button>
+            </Link>
         </nav>
         <div></div>
     </div>
