690d29284b050e998f4555e1e3d7df5188635317
gargar77 <gbautista2@knights.ucf.edu>
2021-07-31 16:25:21 -0400

    remamed page component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,7 @@
  import './homepage.styles.scss';
 
  import Nav from '../../components/nav/nav.component';
- import Artwork from '../artwork/artwork.component';
+ import ArtworkViewer from '../artworkViewer/artworkViewer.component';
  import artworksData from '../../data.json';
  import ArtworkReel from '../../components/artwork-reel/artwork-reel.component';
  import {Route, Switch} from 'react-router-dom'
@@ -15,7 +15,7 @@
                  <Nav/>
                 <Switch>
                     <Route path="/" exact><ArtworkReel artworks={artworksData}/></Route>
-                    <Route path="/artwork"><Artwork artworks={artworksData}/></Route>
+                    <Route path="/artwork"><ArtworkViewer artworks={artworksData}/></Route>
                 </Switch>
              </main>
             
